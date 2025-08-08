af15cbb3317ae9ac14ee18a48bb897cffa5789ad
RazBrry <info@numberwise.nl>
2025-08-08 22:54:39 +0200

    clean test artifacts

--- out-test/src/analyzers/QualityAnalyzer.d.ts
+++
@@ -1,8 +0,0 @@
-import { FileAnalysis, QualityIssue, QualityIssueReport } from '../types/common';
-export declare class QualityAnalyzer {
-    generateQualityReport(analysis: FileAnalysis): QualityIssueReport;
-    extractDetailedIssues(analysis: FileAnalysis): QualityIssue[];
-    private createIssueFromPattern;
-    buildAIInstruction(issues: QualityIssue[], fileName: string): string;
-    private formatIssueSection;
-}
